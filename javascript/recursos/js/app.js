89e67f79a97443f57d6f571533aef19aa36d23c9
Gabriel0070 <161778562+Gabriel0070@users.noreply.github.com>
2024-05-24 21:25:24 -0400

    Add files via upload

---
+++ javascript/recursos/js/app.js
@@ -0,0 +1,15 @@
+const cabecalho= document.getElementById("cabecalho")
+consolo.log(cabecalho)
+
+function pegarDadosDoAluno(){
+const nome=	document.getElementById("nome")
+const nota1=	document.getElementById("nota2")
+const nota2=	document.getElementById("nota2")
+const nota3=	document.getElementById("nota3")
+const nota4=	document.getElementById("nota4")
+
+return nome.value,
+}
+let pegarDados =pegarDadosDoAluno()
+
+console.log("nome", pegarDados)
